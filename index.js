2265b49a8b7bf8ba4867af739c4dbff6438329b2
Dallas <dallas.carlson10@gmail.com>
2025-03-09 22:24:24 -0500

    Add js code to select HTML elements Add shuffle array function to shuffle vals for cases Add Case button functionality to select a shuffled value's index based on case number

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hi");
+const caseBoard = document.querySelector("#caseBoard");
+const valueBoard = document.querySelector("#valueBoard");
+const cases = document.querySelectorAll(".case");
+const values = document.querySelectorAll(".value");
+const instruction = document.getElementById("instructions");
+
+let vals = [...values];
+
+const SHUFFLEPASSES = 3;
+function shuffle() {
+    let pass = SHUFFLEPASSES;
+    while(pass > 0){
+        for(let i = vals.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * (i+1));
+            const temp = vals[i];
+            vals[i] = vals[j];
+            vals[j] = temp;
+        }
+        pass--;
+    }
+
+}
+
+function initButtons() {
+    shuffle();
+    cases.forEach((c) => {
+        c.addEventListener("click", () => {
+            values.forEach((v) => {
+                if(v.id == vals[c.dataset.casenum - 1].id){
+                    v.style.backgroundColor = "gray";
+                }
+            })
+            c.setAttribute("disabled", "true");
+        });
+    })
+}
+
+initButtons();
