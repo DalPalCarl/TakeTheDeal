b8f2913382d94c4e0d2c2f4fc23462038dda9782
Dallas <dallas.carlson10@gmail.com>
2025-03-10 23:51:15 -0500

    Add partial game functionality Add helper functions with game logic Beginning of Calculations for Banker offer

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const cases = document.querySelectorAll(".case");
 const values = document.querySelectorAll(".value");
 const instructionDisplay = document.getElementById("instructions");
 const selectedCase = document.getElementById("caseSelection");
+const decisionButtons = document.getElementById("decisionButtons");
+const takeDealBtn = document.getElementById("takeBtn");
+const leaveDealBtn = document.getElementById("leaveBtn");
 const SHUFFLEPASSES = 3;
+const CASEPICKNUM = 4
+
 
 let firstCase = true;
-let casePicks = 4;
+let casePicks = CASEPICKNUM;
 
 let vals = [...values];
 
@@ -31,24 +36,30 @@ function shuffle() {
     }
 }
 
+function calculateOffer() {
+    let totalVal = 0;
+    const valsLeft = document.querySelectorAll(".value-revealed")
+    valsLeft.forEach(v => totalVal += parseInt(v.id));
+    console.log(totalVal);
+}
+
 function initButtons() {
     shuffle();
+    firstCase = true;
     cases.forEach((c) => {
         c.addEventListener("click", () => {
             handleCaseClick(c);
-            // values.forEach((v) => {
-            //     if(v.id == vals[c.dataset.casenum - 1].id){
-            //         v.style.backgroundColor = "gray";
-            //     }
-            // })
-            // c.setAttribute("disabled", "true");
         });
-    })
+    });
 }
 
 function startGame() {
+    casePicks = CASEPICKNUM;
     loadJson();
     initButtons();
+    takeDealBtn.addEventListener("click", takeDeal);
+    leaveDealBtn.addEventListener("click", leaveDeal);
+    decisionButtons.style.display = "none";
 }
 
 function handleFirstCase(c) {
@@ -57,6 +68,7 @@ function handleFirstCase(c) {
     playerCase.innerText = c.dataset.casenum;
     selectedCase.style.backgroundColor = "white";
     selectedCase.appendChild(playerCase);
+    instructionDisplay.innerText = `Cases to select: ${casePicks}`;
 }
 
 function handleCaseClick(c) {
@@ -66,11 +78,50 @@ function handleCaseClick(c) {
     else{
         values.forEach((v) => {
             if(v.id == vals[c.dataset.casenum - 1].id){
-                v.style.backgroundColor = "gray";
+                v.classList.add("value-revealed");
             }
         });
+        if(casePicks == 1){
+            bankerOffer();
+        }
+        else{
+            casePicks--;
+            instructionDisplay.innerText = `Cases to select: ${casePicks}`;
+        }
     }
     c.setAttribute("disabled", "true");
 }
+
+function bankerOffer() {
+    const offer = 0;
+    calculateOffer();
+    instructionDisplay.innerText = `Banker offers ${offer}`;
+    caseBoard.style.display = "none";
+    decisionButtons.style.display = "inherit";
+}
+
+function takeDeal() {
+    const profit = 0;
+    instructionDisplay.innerText = `Congrats! You won $${profit}!`;
+}
+
+function leaveDeal() {
+    const activeCases = document.querySelectorAll(".case[disabled=true]");
+    if(activeCases.length == 0){
+        revealCase();
+    }
+    else{
+        caseBoard.style.display = "grid";
+        decisionButtons.style.display = "none";
+        casePicks = Math.max(Math.floor((26 - activeCases.length) / 5), 1);
+        instructionDisplay.innerText = `Cases to select: ${casePicks}`;
+    }
+
+}
+
+function revealCase() {
+    instructionDisplay.innerText = `Your case contained $${selectedCase.id}`
+}
+
 
 startGame();
